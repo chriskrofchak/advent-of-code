3b08cd1c9feedd6470e716911b22b62e300f45d1
chriskrofchak <45548165+chriskrofchak@users.noreply.github.com>
2025-12-07 12:40:57 -0500

    ck-7

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,53 +1,47 @@
-import numpy as np
 from solution import Solution, register
-import matplotlib.pyplot as plt
+from copy import deepcopy
+
+def split_beams(prev: str, beamline: str):
+    """
+    Takes a beam line and the previous line and splits the beams
+    """
+    beamlist = [ 0 for _ in beamline ]
+    splits = 0
+    for i, (p, x) in enumerate(zip(prev, beamline)):
+        # base case
+        if p == 'S' or p == '|':
+            beamlist[i] = 1
+
+        if p == '|' and x == '^':
+            splits += 1
+            if i > 0:
+                beamlist[i - 1] = 1
+
+            if i < len(beamline) - 1:
+                beamlist[i + 1] = 1
+
+    return splits, ''.join([ '|' if b == 1 and x != '^' else x for b, x in zip(beamlist, beamline) ])
 
 @register("7_1")
 class Day7(Solution):
 
-    def generate_start(self, layout):
-        beam = np.array([0 if sym == '.' else 1 for sym in layout])
-        return beam
-    
-    def get_next_step(self, layout, current_step):
-        splits = 0
-        is_splitters = [0 if sym == '.' else 1 for sym in layout]
+    def solve(self) -> int:
+        # print(self.input)
+        # for line in self.input:
+        #     print(line)
 
-        next_step = np.copy(current_step)
-        for i in range(len(layout)):
-            if is_splitters[i] and current_step[i]: # if there there is a beam and it hits the splitter
-                next_step[i] = 0
-            
-                # split the beam if there are no blockers (spliters are next to it)
-                if 0 < i and not is_splitters[i - 1]:
-                    next_step[i - 1] = 1
-                if i < len(layout) -1 and not is_splitters[i + 1]:
-                    next_step[i + 1] = 1
+        prev = self.input[0]
+        total = 0
+        outs = [self.input[0]]
+        for line in self.input[1:]:
+            splits, newline = split_beams(prev, line)
+            total += splits
+            prev = newline
+            outs.append(newline)
 
-                    splits += 1
-
-        return np.array(next_step), splits
-    
-    def generate_beam(self):
-        lines = self.input
-        beam = self.generate_start(lines[0])
-
-        total_beam = [beam]
-        total_splits = 0
-
-        for line in lines[1:]:
-            next_beam, splits = self.get_next_step(line, beam)
-
-            total_beam.append(next_beam)
-            total_splits += splits
-
-            beam = next_beam
-            
-        return np.array(total_beam), total_splits
-
-    def solve(self):
-        print(self.input)
-
-        beam, total_splits = self.generate_beam()
-        return total_splits
+        # for line in outs:
+        #     print(line)
+        print(total)
+        return total
+        
         
