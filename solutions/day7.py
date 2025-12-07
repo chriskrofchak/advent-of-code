732e683c520fd691ce5e20275577907ce04696ca
chriskrofchak <45548165+chriskrofchak@users.noreply.github.com>
2025-12-07 12:56:38 -0500

    joined branches, ck-7-2

--- solutions/day7.py
+++ solutions/day7.py
@@ -22,6 +22,30 @@ def split_beams(prev: str, beamline: str):
 
     return splits, ''.join([ '|' if b == 1 and x != '^' else x for b, x in zip(beamlist, beamline) ])
 
+def make_newline(beamlist: list[int], beamline: str):
+    return ''.join([ '|' if b == 1 and x != '^' else x for b, x in zip(beamlist, beamline) ])
+
+def split_beams_nd(prev: str, beamlines: str):
+    if len(beamlines) == 0:
+        return 1
+
+    beamline = beamlines[0]
+    lbeam, rbeam = ([ 0 for _ in beamline ], [ 0 for _ in beamline ])
+    for i, (p, x) in enumerate(zip(prev, beamline)):
+        # base case
+        if p == 'S' or p == '|':
+            lbeam[i] = 1
+            rbeam[i] = 1 
+
+        if p == '|' and x == '^':
+            if i > 0:
+                lbeam[i - 1] = 1
+
+            if i < len(beamline) - 1:
+                rbeam[i + 1] = 1
+
+    return split_beams_nd(make_newline(lbeam, beamline), beamlines[1:]) + split_beams_nd(make_newline(rbeam, beamline), beamlines[1:]) 
+
 @register("7_1")
 class Day7(Solution):
 
@@ -43,5 +67,14 @@ class Day7(Solution):
         #     print(line)
         print(total)
         return total
+        
+@register("7_2")
+class Day7(Solution):
+
+    def solve(self) -> int:
+        # print(self.input)
+        # for line in self.input:
+        #     print(line)
+        return split_beams_nd(self.input[0], self.input[1:])
         
         
